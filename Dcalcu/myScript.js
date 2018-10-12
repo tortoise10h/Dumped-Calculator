b4f0206ab1dfa1be73f2497ed41bec47ac5e5adf
Huy Nguyen <35460173+tortoise10h@users.noreply.github.com>
2018-10-12 11:15:52 +0700

    update myScript.js
    add script for calculator

--- Dcalcu/myScript.js
+++ Dcalcu/myScript.js
@@ -1,10 +1,44 @@
-var num1;
-var num2;
-function roundFunction(){
-    num1 = document.getElementById("number1").value;
-    if(num1 == 0){
-        num1 = document.getElementById("number2").value;
+// insert value by button
+var keyboard = document.getElementById("keyboard");
+console.log(keyboard);
+keyboard.addEventListener('click', insert);
+
+function insert(e){
+    if(e.target.classList.contains('btn')){
+        //take value from click
+        var value = e.target.value; 
+        //if values are numbers or operators
+        if(value !== "←" && value !== "C" && value !== "="){
+            //handle print x on screen
+            if(value == "x"){
+                document.querySelector("#screen #output-screen").value += "*";
+            }else{
+                document.querySelector("#screen #output-screen").value += value;
+            }
+        }else if(value === "="){
+            document.querySelector("#screen #output-screen").value = result(document.querySelector("#screen #output-screen").value);
+        }else if(value === "C"){   
+            document.querySelector("#screen #output-screen").value = clearAll(document.querySelector("#screen #output-screen").value);
+        }else if(value === "←"){
+            document.querySelector("#screen #output-screen").value = clearOne(document.querySelector("#screen #output-screen").value);
+        }
     }
-    var result = Math.round(num1);
-    document.getElementById("result").innerHTML = result;
+}
+//for = button
+function result(value){
+   if(value){
+        var output = eval(value);
+        return output;
+   }
+   return "";
+}
+//for C button
+function clearAll(text){
+    text = "";
+    return text;
+}
+//for <- button
+function clearOne(text){
+    text = text.substring(0,text.length-1);
+    return text;
 }
